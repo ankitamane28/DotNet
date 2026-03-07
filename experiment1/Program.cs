11186f9fa8d55edc0cdfbffe4c940c262a0e250a
Ankita Mane <maneanku451@gmail.com>
2026-03-07 23:20:27 +0530

    exp1

---
+++ experiment1/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+namespace Exp01_Arithmetic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 7;
+            int b = 8;
+
+            Console.WriteLine("Addition: " + (a + b));
+            Console.WriteLine("Subtraction: " + (a - b));
+            Console.WriteLine("Multiplication: " + (a * b));
+            Console.WriteLine("division: " + (a / b));
+        }
+    }
+}
