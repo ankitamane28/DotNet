76a5e4427776f7d0571723f43f2d64e272a95ba1
Ankita Mane <maneanku451@gmail.com>
2026-03-08 21:40:34 +0530

    exp2

---
+++ experiment2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Student
+{
+    // Data Members (private for encapsulation)
+    private int rollNo;
+    private string name;
+
+    // Method to set values
+    public void SetData(int r, string n)
+    {
+        rollNo = r;
+        name = n;
+    }
+
+    // Method to display values
+    public void DisplayData()
+    {
+        Console.WriteLine("Roll No: " + rollNo);
+        Console.WriteLine("Name: " + name);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Creating object
+        Student s1 = new Student();
+
+        // Calling method to set data
+        s1.SetData(1, "Ankita");
+
+        // Calling method to display data
+        Console.WriteLine("Student Details:");
+        s1.DisplayData();
+
+        Console.ReadLine();
+    }
+}
